b80b8f29c0283f59b69e257a3f8505aa1b8567f0
MariaVSoto <sotomaria2304@gmail.com>
2025-04-09 03:05:55 +0000

    Add folder template

---
+++ Terraform/modules/webserver/variables.tf
@@ -0,0 +1,35 @@
+variable "environment" {
+  description = "Environment name (prod)"
+  type        = string
+}
+
+variable "team_name" {
+  description = "Name of the team"
+  type        = string
+}
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnets" {
+  description = "List of public subnet CIDRs"
+  type        = list(string)
+}
+
+variable "private_subnets" {
+  description = "List of private subnet CIDRs"
+  type        = list(string)
+}
+
+variable "availability_zones" {
+  description = "List of availability zones"
+  type        = list(string)
+
+}
