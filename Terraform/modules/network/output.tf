007c4d5534af763cfb1cb3b89d2f44ff62e236f6
MariaVSoto <sotomaria2304@gmail.com>
2025-04-10 15:56:24 +0000

    update outputs

--- Terraform/modules/network/output.tf
+++ Terraform/modules/network/output.tf
@@ -1,3 +1,15 @@
 output "vpc_id" {
   value = aws_vpc.VPC.id
 }
+
+output "cidr_block" {
+  value = aws_vpc.VPC.cidr_block
+}
+
+output "PublicSubnet" {
+  value = aws_subnet.public[*].id
+}
+
+output "PrivateSubnet" {
+  value = aws_subnet.private[*].id
+}
