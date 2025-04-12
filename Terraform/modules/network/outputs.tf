112887216287723ff979cd4f99c71cbf41ededa1
Kevin Wang <kevinhust@gmail.com>
2025-04-11 21:31:25 -0400

    更新Webserver模块以支持新架构
    合并PR #19：更新Webserver模块以支持新的基础设施架构
    
    主要更改包括：
    - 网络模块优化
    - ALB模块添加
    - Webserver模块重构
    - 变量和输出更新
    - 安全组规则完善
    
    所有更改已经过测试和验证，符合架构要求。

--- Terraform/modules/network/outputs.tf
+++ Terraform/modules/network/outputs.tf
@@ -1,24 +1,39 @@
 output "vpc_id" {
-  description = "ID of the VPC"
+  description = "ID of the created VPC"
   value       = aws_vpc.VPC.id
 }
 
+output "cidr_block" {
+  description = "CIDR block of the created VPC"
+  value       = aws_vpc.VPC.cidr_block
+}
+
 output "public_subnet_ids" {
-  description = "IDs of the public subnets"
-  value       = aws_subnet.public[*].id
+  description = "IDs of the created public subnets"
+  value       = { for i, subnet in aws_subnet.public : "public-${i + 1}" => subnet.id }
 }
 
 output "private_subnet_ids" {
-  description = "IDs of the private subnets"
-  value       = aws_subnet.private[*].id
+  description = "IDs of the created private subnets"
+  value       = { for i, subnet in aws_subnet.private : "private-${i + 1}" => subnet.id }
 }
 
-output "bastion_sg_id" {
-  description = "ID of the bastion host security group"
+output "web_security_group_id" {
+  description = "ID of the web security group"
+  value       = aws_security_group.web_sg.id
+}
+
+output "alb_security_group_id" {
+  description = "ID of the ALB security group"
+  value       = aws_security_group.alb_sg.id
+}
+
+output "bastion_security_group_id" {
+  description = "ID of the bastion security group"
   value       = aws_security_group.bastion_sg.id
 }
 
-output "private_sg_id" {
-  description = "ID of the private subnet security group"
+output "private_security_group_id" {
+  description = "ID of the private security group"
   value       = aws_security_group.private_sg.id
 } 
