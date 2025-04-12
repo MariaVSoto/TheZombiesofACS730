021af9ac7fc24f9f65ff37ead39bd102a73b6b1c
Kevin Wang <kevinhust@gmail.com>
2025-04-11 21:40:59 -0400

    Optimize webserver setup script: Simplify image download logic, improve display name setting, update S3 bucket path

--- Terraform/modules/webserver/setup_webserver.sh
+++ Terraform/modules/webserver/setup_webserver.sh
@@ -23,16 +23,15 @@ INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 # Check if instance is part of an Auto Scaling group
 ASG_NAME=$(aws autoscaling describe-auto-scaling-instances --instance-ids $INSTANCE_ID --region us-east-1 --query 'AutoScalingInstances[0].AutoScalingGroupName' --output text)
 
-# Download appropriate image based on ASG status
+# Set display name based on ASG status
 if [ "$ASG_NAME" != "None" ] && [ ! -z "$ASG_NAME" ]; then
-    # Instance is part of ASG
-    aws s3 cp s3://${S3_BUCKET}/webserver/ASG.jpg /var/www/html/server.jpg
     DISPLAY_NAME="ASG Instance: $ASG_NAME"
 else
-    # Standalone instance
-    aws s3 cp s3://${S3_BUCKET}/webserver/webserver${WEBSERVER_ID}.jpg /var/www/html/server.jpg
     DISPLAY_NAME="Webserver ${WEBSERVER_ID}"
 fi
+
+# Copy ASG image from module directory
+aws s3 cp s3://${S3_BUCKET}/modules/webserver/ASG.jpg /var/www/html/server.jpg
 
 # Create index.html with hostname, IP, creation method, and image
 cat <<EOT | sudo tee /var/www/html/index.html
