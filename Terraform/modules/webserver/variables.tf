cb5b7ce9606bdc6dc61e1d71f10c912650caf9c2
Kevin Wang <kevinhust@gmail.com>
2025-04-13 21:36:17 -0400

    feat: Restructure Terraform configuration for multi-environment support

--- Terraform/modules/webserver/variables.tf
+++ Terraform/modules/webserver/variables.tf
@@ -4,8 +4,8 @@ variable "team_name" {
 }
 
 variable "region" {
-  type        = string
   description = "AWS region"
+  type        = string
 }
 
 variable "common_tags" {
@@ -46,6 +46,11 @@ variable "alb_security_group_id" {
 variable "key_name" {
   type        = string
   description = "Name of the SSH key pair"
+}
+
+variable "public_key" {
+  description = "SSH public key"
+  type        = string
 }
 
 variable "asg_desired_capacity" {
@@ -105,4 +110,10 @@ variable "additional_tags" {
   type        = map(string)
   description = "Additional tags to be applied to all resources"
   default     = {}
+}
+
+variable "admin_ip_cidr" {
+  description = "CIDR block for administrative access"
+  type        = string
+  default     = "0.0.0.0/0" # Consider restricting this to your IP
 }
