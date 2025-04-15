9f2e8f6e73c0b163bd2e616c112c5e1b74ac6bd5
MariaVSoto <sotomaria2304@gmail.com>
2025-04-14 21:21:58 -0400

    copy (#54)
    Co-authored-by: Shruti Vasanwala <159313092+Shrutii-30@users.noreply.github.com>

--- Terraform/modules/webserver/main.tf
+++ Terraform/modules/webserver/main.tf
@@ -21,7 +21,7 @@ resource "aws_security_group" "web_sg" {
     from_port       = 22
     to_port         = 22
     protocol        = "tcp"
-    cidr_blocks = [var.vpc_cidr_block] 
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
